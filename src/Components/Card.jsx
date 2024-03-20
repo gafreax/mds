6399d70dea821880a82e3197977b0b3065451149
Sossio <sandercohen10@gmail.com>
2024-03-20 17:58:55 +0100

    ho creato una card

---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import "./Card.css"
+
+function Card() {
+    return (
+    <div className="Card">
+        <img src="/SimpleCode.jpg"  className="Img"/>
+        <div className="card-body">
+        <h5 className="Text">Code</h5>
+        </div>
+    </div>
+    );
+}
+
+export default Card
